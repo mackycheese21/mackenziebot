5c4a685157a9a6479ef87f878cc32518450e07a2
Mackenzie Armstrong <jearmstrong21@gmail.com>
2020-10-29 22:29:32 -0700

    it no compile :(

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::convert::{TryFrom, TryInto};
 
 struct Dice {
     count: u32,
@@ -8,7 +9,17 @@ struct Dice {
 
 impl Dice {
     fn generate(&self) -> Vec<u32> {
-        unimplemented!()
+        let mut v = vec![];
+        for _ in 0..self.count {
+            v.push(rand::thread_rng().gen_range(1, self.count + 1))
+        }
+        v.sort();
+        if self.drop < 0 {
+            v.drain(..-self.drop)
+        } else if self.drop > 0 {
+            v.drain(v.len() - self.drop..)
+        }
+        v
     }
 }
 
@@ -21,28 +32,51 @@ enum Component {
 
 type Args = Vec<Component>;
 
-impl From<&str> for Option<Dice> {
-    fn from(_: &str) -> Self {
+type ParseOutput<'a, T> = (&'a str, T);
+
+impl<'a> TryFrom<&'a str> for ParseOutput<'a, Dice> {
+    type Error = ();
+
+    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
         unimplemented!()
     }
 }
 
-impl From<&str> for Option<Bonus> {
-    fn from(_: &str) -> Self {
+impl<'a> TryFrom<&'a str> for ParseOutput<'a, Bonus> {
+    type Error = ();
+
+    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
         unimplemented!()
     }
 }
 
-impl From<&str> for Option<Component> {
-    fn from(_: &str) -> Self {
+impl<'a> TryFrom<&'a str> for ParseOutput<'a, Component> {
+    type Error = ();
+
+    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
+        if let Ok((value, dice)) = value.try_into() {
+            Ok((value, Component::Dice(dice)))
+        } else if let Ok((value, bonus)) = value.try_into() {
+            Ok((value, Component::Bonus(bonus)))
+        } else {
+            Err(())
+        }
+    }
+}
+
+impl<'a> TryFrom<&'a str> for ParseOutput<'a, Args> {
+    type Error = ();
+
+    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
         unimplemented!()
     }
 }
 
-impl From<&str> for Option<Args> {
-    fn from(_: &str) -> Self {
-        unimplemented!()
-    }
+fn main() {
+    let d = Dice {
+        count: 10,
+        max: 10,
+        drop: 2,
+    };
+    println!("{:?}", d.generate())
 }
-
-fn main() {}
